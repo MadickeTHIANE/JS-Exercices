5563b6d2ac6396762e544b2317e05d6f26435a98
Madické THIANE <madicke.thiane@gmail.com>
2021-06-12 00:25:25 +0200

    Conversion chiffre arabe en chiffre romains - 3éme méthode

--- 00-facile/exercice-4/exercice-4.js
+++ 00-facile/exercice-4/exercice-4.js
@@ -1,72 +1,30 @@
-// Méthode 2
+// Méthode 3
+
 function exercice4(nombre) {
         var chiffreRomain = "";
       
-        while (nombre >= 9000) {
-          chiffreRomain += "XM|";
-          nombre -= 9000;
+        function calculChiffreRomain(chiffreArabe, lettreRomaine) {
+          while (nombre >= chiffreArabe) {
+            chiffreRomain += lettreRomaine;
+            nombre -= chiffreArabe;
+          }
         }
-        while (nombre >= 1000) {
-          nombre -= 1000;
-          chiffreRomain += "M";
-        }
-      
-        while (nombre >= 900) {
-          chiffreRomain += "CM";
-          nombre -= 900;
-        }
-        while (nombre >= 500) {
-          nombre -= 500;
-          chiffreRomain += "D";
-        }
-      
-        while (nombre >= 400) {
-          chiffreRomain += "CD";
-          nombre -= 400;
-        }
-        while (nombre >= 100) {
-          nombre -= 100;
-          chiffreRomain += "C";
-        }
-      
-        while (nombre >= 90) {
-          chiffreRomain += "XC";
-          nombre -= 90;
-        }
-        while (nombre >= 50) {
-          nombre -= 50;
-          chiffreRomain += "L";
-        }
-      
-        while (nombre >= 40) {
-          chiffreRomain += "XL";
-          nombre -= 40;
-        }
-        while (nombre >= 10) {
-          nombre -= 10;
-          chiffreRomain += "X";
-        }
-      
-        while (nombre >= 9) {
-          chiffreRomain += "IX";
-          nombre -= 9;
-        }
-        while (nombre >= 5) {
-          nombre -= 5;
-          chiffreRomain += "V";
-        }
-      
-        while (nombre >= 4) {
-          chiffreRomain += "IV";
-          nombre -= 4;
-        }
-        while (nombre >= 1) {
-          nombre -= 1;
-          chiffreRomain += "I";
-        }
+        calculChiffreRomain(9000, "WM|");
+        calculChiffreRomain(1000, "M");
+        calculChiffreRomain(900, "CM");
+        calculChiffreRomain(500, "D");
+        calculChiffreRomain(400, "CD");
+        calculChiffreRomain(100, "C");
+        calculChiffreRomain(90, "XC");
+        calculChiffreRomain(50, "L");
+        calculChiffreRomain(40, "XL");
+        calculChiffreRomain(10, "X");
+        calculChiffreRomain(9, "IX");
+        calculChiffreRomain(5, "V");
+        calculChiffreRomain(4, "IV");
+        calculChiffreRomain(1, "I");
       
         return chiffreRomain;
-      }
-      
+      }      
 // Ne touchez pas à la ligne suivante
 module.exports = exercice4;
