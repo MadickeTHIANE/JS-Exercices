8493929c863e7c2c2cc965e16a8e18ad7a2877f6
Madické THIANE <madicke.thiane@gmail.com>
2021-06-12 00:38:26 +0200

    Conversion chiffre arabe en chiffre romains - 4éme méthode

--- 00-facile/exercice-4/exercice-4.js
+++ 00-facile/exercice-4/exercice-4.js
@@ -1,30 +1,36 @@
-// Méthode 3
+// Méthode 4
 
 function exercice4(nombre) {
-        var chiffreRomain = "";
-      
-        function calculChiffreRomain(chiffreArabe, lettreRomaine) {
-          while (nombre >= chiffreArabe) {
-            chiffreRomain += lettreRomaine;
-            nombre -= chiffreArabe;
-          }
-        }
-        calculChiffreRomain(9000, "WM|");
-        calculChiffreRomain(1000, "M");
-        calculChiffreRomain(900, "CM");
-        calculChiffreRomain(500, "D");
-        calculChiffreRomain(400, "CD");
-        calculChiffreRomain(100, "C");
-        calculChiffreRomain(90, "XC");
-        calculChiffreRomain(50, "L");
-        calculChiffreRomain(40, "XL");
-        calculChiffreRomain(10, "X");
-        calculChiffreRomain(9, "IX");
-        calculChiffreRomain(5, "V");
-        calculChiffreRomain(4, "IV");
-        calculChiffreRomain(1, "I");
-      
-        return chiffreRomain;
-      }      
+  var chiffreRomain = "";
+
+  conversions =[
+    [9000, "WM|"],
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+]
+
+  function calculChiffreRomain(chiffreArabe, lettreRomaine) {
+    while (nombre >= chiffreArabe) {
+      chiffreRomain += lettreRomaine;
+      nombre -= chiffreArabe;
+    }
+  }
+
+  conversions.forEach((conversion) => {
+    calculChiffreRomain(conversion[0], conversion[1])
+  });
+  return chiffreRomain;
+}
 // Ne touchez pas à la ligne suivante
 module.exports = exercice4;
